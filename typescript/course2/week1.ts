d2fc0795b8f184813f9e32ab773f80db4fc47abd
jeffsolo <jeffreycsolomon@gmail.com>
2019-02-16 22:44:23 +0100

    start week1 solution, include output folder

--- typescript/course2/week1.ts
+++ typescript/course2/week1.ts
@@ -1,4 +1,9 @@
 // import DNA from '../bioinformatics/DNA';
+import {IDataFile, parseDataFile, saveToFile} from '../bioinformatics/CourseHelper';
 import {stringComposition} from '../bioinformatics/Reconstructor';
 
-console.log(stringComposition('ABCDE', 3));
+const dataPath = './datasets/week1';
+const outPath = './output/week1';
+
+const {body: sequence, header: k}: IDataFile = parseDataFile(`${dataPath}/dataset_197_3.txt`, {hasHeader: true});
+saveToFile(`${outPath}/stringComp.txt`, stringComposition(sequence, Number(k)), true);
