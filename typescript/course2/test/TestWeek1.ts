f23edfa2ada58d63169a3ed58f9b20dfe4aa860f
jeffsolo <jeffreycsolomon@gmail.com>
2019-02-19 15:57:47 +0100

    multiple test cases for string composition

--- typescript/course2/test/TestWeek1.ts
+++ typescript/course2/test/TestWeek1.ts
@@ -1,11 +1,21 @@
 import assert from 'assert';
+import fs from 'fs';
+import {IDataFile, parseDataFile} from '../../bioinformatics/CourseHelper';
 import {stringComposition} from '../../bioinformatics/Reconstructor';
 
-describe('Week 1 test', () => {
-    it('decompose strand', () => {
-        const input = 'CAATCCAAC';
-        const size = 5;
-        const expected = ['CAATC', 'AATCC', 'ATCCA', 'TCCAA', 'CCAAC'];
-        assert.deepEqual(stringComposition(input, size), expected);
+const baseTestInputPath = './typescript/course2/test/inputs';
+const baseTestOutputPath = './typescript/course2/test/outputs';
+
+describe('Week 1 Test', () => {
+  ['test1.txt', 'test2.txt', 'test3.txt', 'test4.txt'].forEach((fileName) => {
+    it(`should decompose strand in file ${fileName}`, () => {
+      const inputPath = `${baseTestInputPath}/kmerComposition`;
+      const outputPath = `${baseTestOutputPath}/kmerComposition`;
+      const {body, header}: IDataFile = parseDataFile(`${inputPath}/${fileName}`, {hasHeader: true});
+      const actualOutput = stringComposition(body, Number(header));
+      const expectedOutput = fs.readFileSync(`${outputPath}/${fileName}`).toString().split('\n');
+
+      assert.deepEqual(actualOutput, expectedOutput);
     });
+  });
 });
